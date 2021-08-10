0188bf0790e639a3a0eaf2e2a3f26fc0d9b15020
nstrong <nathanstrong.fx@gmail.com>
2021-08-10 08:17:35 -0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+    const expenses = [
+        {title: 'Car Insurance', amount: 295.65, date: new Date(2021, 8, 10)},
+        {title: 'Cat Food', amount: 45, date: new Date(2021, 8, 9)},
+        {title: 'Gold Grill', amount: 1800, date: new Date(2021, 8, 9)},
+        {title: 'Gold Chain', amount: 967, date: new Date(2021, 7, 27)},
+    ]
+
     return (
         <div>
             <h2>Let's get started!</h2>
-            <ExpenseItem />
+            {expenses.map(expense => <ExpenseItem key={`${expense.date.toString()}-${expense.title}`} {...expense}/>)}
         </div>
     );
 }
--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,5 +1,35 @@
-function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+import './ExpenseItems.css';
+
+function localizeCurrency(value: number) {
+    const formatter = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
+    return formatter.format(value);
+}
+
+interface ExpenseProps {
+    title: string,
+    date: Date,
+    amount: number,
+}
+
+function ExpenseItem({title, date, amount}: ExpenseProps) {
+    const month = date.toLocaleDateString('en-US', {month: 'short'});
+    const day = date.toLocaleDateString('en-US', {day: 'numeric'});
+    const year = date.toLocaleDateString('en-US', {year: 'numeric'});
+
+    return (<div className='expense-item'>
+
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
+
+            <div className='expense-item__description'>
+                <h2>{title}</h2>
+                <div className='expense-item__price'>{localizeCurrency(amount)}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;

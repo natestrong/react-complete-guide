5f40b5d7cf128ec9561ab9221bc64648e5d831b6
nstrong <nathanstrong.fx@gmail.com>
2021-08-11 19:17:12 -0700

    init

--- src/components/NewExpense/ExpenseForm.tsx
+++ src/components/NewExpense/ExpenseForm.tsx
@@ -1,30 +1,48 @@
 import './ExpenseForm.css';
 import {SyntheticEvent, useState} from 'react';
 
+export interface INewExpenseForm {
+    title: string;
+    dateString: string;
+    date: Date;
+    amount: number;
+}
+
 function ExpenseForm() {
-    const [userInput, setUserInput] = useState({
+    const [userInput, setUserInput] = useState<INewExpenseForm>({
         title: '',
-        date: new Date().toISOString().split('T')[0],
+        date: new Date(),
+        dateString: new Date().toISOString().split('T')[0],
         amount: 0
     });
 
     function titleChangeHandler(e: SyntheticEvent<HTMLInputElement>) {
         const title = (e.target as HTMLInputElement).value;
-        setUserInput({...userInput, title});
+        setUserInput((prevState) => ({...prevState, title}));
     }
 
     function amountChangeHandler(e: SyntheticEvent<HTMLInputElement>) {
         const amount = Number((e.target as HTMLInputElement).value);
-        setUserInput({...userInput, amount})
+        setUserInput((prevState) => ({...prevState, amount}));
     }
 
     function dateChangeHandler(e: SyntheticEvent<HTMLInputElement>) {
-        const date = (e.target as HTMLInputElement).value;
-        setUserInput({...userInput, date});
+        const dateString = (e.target as HTMLInputElement).value;
+        setUserInput((prevState) => ({
+            ...prevState,
+            dateString,
+            date: new Date(dateString)
+        }));
+    }
+
+    function submitHandler(e: SyntheticEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        console.log(userInput);
     }
 
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label htmlFor='titleInput'>Title</label>
@@ -46,7 +64,7 @@ function ExpenseForm() {
                     <label htmlFor='dateInput'>Date</label>
                     <input id='dateInput'
                            onChange={dateChangeHandler}
-                           value={userInput.date}
+                           value={userInput.dateString}
                            type='date'
                            min='2019-01-01'
                            max='2022-12-31'/>
